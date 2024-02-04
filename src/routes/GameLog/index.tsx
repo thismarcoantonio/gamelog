3c7e59afff32074cdf24dfa534fe104a49f549b1
thismarcoantonio <thismarcoantonio@gmail.com>
2024-02-03 22:39:13 -0800

    Show results on GameLog

--- src/routes/GameLog/index.tsx
+++ src/routes/GameLog/index.tsx
@@ -1,16 +1,32 @@
+import { Fragment } from "react";
 import { GameLogHeading } from "./GameLogHeading";
 import { Section } from "../../components/Section";
+import { GameCard } from "../../components/GameCard";
 import styles from "./index.module.css";
 import sample from "../../data/sample";
+import { GameRow } from "../../components/GameRow";
+import { Divider } from "../../components/Divider";
 
 export function GameLog() {
   return (
     <div>
       <GameLogHeading image={sample.results[0].background_image} />
       <Section title="Currently playing" className={styles.currentlyPlaying} noGutters>
-        Hello
+        {/* TODO: Remove this slice when we get real data */}
+        <div className={styles.currentlyPlayingResults}>
+          {sample.results.slice(0, 4).map((result) => (
+            <GameCard title={result.name} image={result.background_image} key={result.id} />
+          ))}
+        </div>
       </Section>
-      Gamelog
+      <Section title="Completed" className={styles.completed}>
+        {sample.results.map((result, index) => (
+          <Fragment key={result.id}>
+            {index > 0 && <Divider />}
+            <GameRow title={result.name} image={result.background_image} dateRelease={result.released} dateCompleted={result.user_game.added} />
+          </Fragment>
+        ))}
+      </Section>
     </div>
   );
 }
