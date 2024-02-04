4c28bd35d1af77d16d2c765daac2f3b1853ec8a9
thismarcoantonio <thismarcoantonio@gmail.com>
2024-02-03 22:38:34 -0800

    Update section component to have padding on title

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 export function Section({ title, className, noGutters, children }: Props) {
   return (
     <section className={cn(styles.section, className, { [styles.sectionPadding]: !noGutters })}>
-      <h2 className={cn(styles.title, { [styles.titlePadding]: noGutters })}>{title}</h2>
+      <h1 className={cn(styles.title, { [styles.titlePadding]: noGutters })}>{title}</h1>
       {children}
     </section>
   );
